0e029c5c0c2f2b41d17a7b21cae74ff4372ccf59
kenoll <kelseynoll@gmail.com>
2017-12-13 15:57:38 -0500

    added in gsub to change longer strain designations (CC001/Unc) to shorter ones (CC001) so they work in line2alias

--- R/line2alias.R
+++ R/line2alias.R
@@ -18,6 +18,10 @@ CC_names_l2a[1:2] <- sapply(CC_names_l2a[1:2], as.character)
 
 alias2line <- function(df){
 
+  if(!any((c("dam","RIX","Strain","strain","Line","line") %in% colnames(df)))){
+    stop('column name must have either "dam", "RIX", "Strain", "strain", "Line", "line"')
+    }
+
     alias2line.rixbase <- function(df){
       df$dam <- as.character(df$dam)
       df$sire <- as.character(df$sire)
@@ -77,7 +81,11 @@ alias2line <- function(df){
 
 line2alias <- function(df){
 
-  line2aliasbase <- function(df){
+  if(!any((c("dam","RIX","Strain","strain","Line","line") %in% colnames(df)))){
+    stop('column name must have either "dam", "RIX", "Strain", "strain", "Line", "line"')
+  }
+
+  line2alias.rixbase <- function(df){
 
     df$dam <- as.character(df$dam)
     df$sire <- as.character(df$sire)
@@ -95,16 +103,34 @@ line2alias <- function(df){
     df["RIX"] <- paste(df$dam,df$sire,sep="x")
 
     return(df)
+     }
+
+    line2alias.base <- function(df,mice){
+      df[mice] <- as.character(df[[mice]])
+      colnames(CC_names_l2a)[1] <- mice
+      df <- merge(df,CC_names_l2a[1:2],by=mice,all.x=T)
+      df[mice] <- df["Alias"]
+      df <- df[-length(df)]
     }
 
-  if(any("RIX" %in% colnames(df)) && any(!("dam" %in% colnames(df)))) {
-    df <- rixsplit(df)
-    df <- line2aliasbase(df)
-    df <- df[-(1:2)]
-  } else {
-    if(any("dam" %in% colnames(df))){
-      df <- line2aliasbase(df)
+    if(any("RIX" %in% colnames(df)) && any(!("dam" %in% colnames(df)))) {
+      df <- rixsplit(df)
+      df <- line2alias.rixbase(df)
+      df <- df[-(1:2)]
+    } else if(any("dam" %in% colnames(df))){
+      df <- line2alias.rixbase(df)
+    } else if(any("Strain" %in% colnames(df))){
+      df$Strain <- gsub("/.*","",df$Strain)
+      df <- line2alias.base(df,"Strain")
+    } else if(any("strain" %in% colnames(df))){
+      df$strain <- gsub("/.*","",df$strain)
+      df <- line2alias.base(df,"strain")
+    } else if(any("Line" %in% colnames(df))){
+      df$Line <- gsub("/.*","",df$Line)
+      df <- line2alias.base(df,"Line")
+    } else if(any("line" %in% colnames(df))){
+      df$line <- gsub("/.*","",df$line)
+      df <- line2alias.base(df,"line")
     }
-  }
-  return(df)
+    return(df)
 }
