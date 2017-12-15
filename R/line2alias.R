aaf105135d7051dd1d71bb8ff4cd6db097d0301e
kenoll <kelseynoll@gmail.com>
2017-12-15 12:35:53 -0500

    Added input column for "strain.col" and "is.rix" to streamline function - user now inputs name of the column that has the strain name to be swapped.

--- R/line2alias.R
+++ R/line2alias.R
@@ -1,110 +1,111 @@
 #' @title Alias 2 Line
-#'
 #' @description Old CC Alias to New CC Line
 #'
 #' @param df
+#' @param strain.col Character, name of column containing strains to be changed- not required if is.rix=T.
+#' @param is.rix Logical, is the data from inbred crosses (RIX)?
 #'
 #' @return df
 #'
 #' @examples
-#'
 #' @export
 
-alias2line <- function(df){
 
-  if(!any((c("dam","RIX","Strain","strain","Line","line") %in% colnames(df)))){
-    stop('column name must have either "dam", "RIX", "Strain", "strain", "Line", "line"')
-    }
+alias2line <- function(df,strain.col,is.rix=F){
 
-    alias2line.rixbase <- function(df){
-      df$dam <- as.character(df$dam)
-      df$sire <- as.character(df$sire)
+  if (is.rix == F) {
+    df[strain.col] <- as.character(df[[strain.col]])
+    colnames(CC_names_a2l)[1] <- strain.col
+    df <- merge(df,CC_names_a2l[1:2],by=strain.col,all.x=T)
+    df[strain.col] <- df["CCLine"]
+    df <- df[-length(df)]
+    return(df)
 
-      colnames(CC_names_a2l)[1] <- "sire"
-      df <- merge(df,CC_names_a2l[1:2],by="sire",all.x=T)
-      df["sire"] <- df["CCLine"]
-      df <- df[-length(df)]
+  } else if (is.rix ==T) {
 
-      colnames(CC_names_a2l)[1]="dam"
-      df=merge(df,CC_names_a2l[1:2],by="dam",all.x=T)
-      df["dam"]=df["CCLine"]
-      df=df[-length(df)]
+      alias2line.rixbase <- function(df){
+        df$dam <- as.character(df$dam)
+        df$sire <- as.character(df$sire)
 
-      df["RIX"]=paste(df$dam,df$sire,sep="x")
-      return(df)
-    }
+        colnames(CC_names_a2l)[1] <- "sire"
+        df <- merge(df,CC_names_a2l[1:2],by="sire",all.x=T)
+        df["sire"] <- df["CCLine"]
+        df <- df[-length(df)]
 
-    alias2line.base <- function(df,mice){
-      df[mice] <- as.character(df[[mice]])
-      colnames(CC_names_a2l)[1] <- mice
-      df <- merge(df,CC_names_a2l[1:2],by=mice,all.x=T)
-      df[mice] <- df["CCLine"]
-      df <- df[-length(df)]
-    }
+        colnames(CC_names_a2l)[1]="dam"
+        df=merge(df,CC_names_a2l[1:2],by="dam",all.x=T)
+        df["dam"]=df["CCLine"]
+        df=df[-length(df)]
 
-  if(any("RIX" %in% colnames(df)) && any(!("dam" %in% colnames(df)))) {
-    df <- rixsplit(df)
-    df <- alias2line.rixbase(df)
-    df <- df[-(1:2)]
-    } else if(any("dam" %in% colnames(df))){
+        df["RIX"]=paste(df$dam,df$sire,sep="x")
+        return(df)
+      }
+
+      if (any("RIX" %in% colnames(df)) && any(!("dam" %in% colnames(df)))) {
+        df <- rixsplit(df)
         df <- alias2line.rixbase(df)
-        } else if(any("Strain" %in% colnames(df))){
-            df <- alias2line.base(df,"Strain")
-        } else if(any("strain" %in% colnames(df))){
-          df <- alias2line.base(df,"strain")
-        } else if(any("Line" %in% colnames(df))){
-          df <- alias2line.base(df,"Line")
-        } else if(any("line" %in% colnames(df))){
-          df <- alias2line.base(df,"line")
+        df <- df[-(1:2)]
+        } else if (any("dam" %in% colnames(df))){
+          df <- alias2line.rixbase(df)
         }
-  return(df)
+        return(df)
+  }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 #' @title Line 2 Alias
-#'
 #' @description New CC Line to Old CC Alias
 #'
 #' @param df
+#' @param strain.col Character, name of column containing strains to be changed- not required if is.rix=T.
+#' @param is.rix Logical, is the data from inbred crosses (RIX)?
 #'
 #' @return df
 #'
 #' @examples
-#'
 #' @export
 
+line2alias <- function(df,strain.col,is.rix=F){
+  if (is.rix == F) {
 
-line2alias <- function(df){
-
-  if(!any((c("dam","RIX","Strain","strain","Line","line") %in% colnames(df)))){
-    stop('column name must have either "dam", "RIX", "Strain", "strain", "Line", "line"')
-  }
-
-  line2alias.rixbase <- function(df){
-
-    df$dam <- as.character(df$dam)
-    df$sire <- as.character(df$sire)
-
-    colnames(CC_names_l2a)[1] <- "sire"
-    df <- merge(df,CC_names_l2a[1:2],by="sire",all.x=T)
-    df["sire"] <- df["Alias"]
+    df[strain.col] <- as.character(df[[strain.col]])
+    colnames(CC_names_l2a)[1] <- strain.col
+    df <- merge(df,CC_names_l2a[1:2],by=strain.col,all.x=T)
+    df[strain.col] <- df["Alias"]
     df <- df[-length(df)]
-
-    colnames(CC_names_l2a)[1] <- "dam"
-    df <- merge(df,CC_names_l2a[1:2],by="dam",all.x=T)
-    df["dam"] <- df["Alias"]
-    df <- df[-length(df)]
-
-    df["RIX"] <- paste(df$dam,df$sire,sep="x")
-
     return(df)
-     }
 
-    line2alias.base <- function(df,mice){
-      df[mice] <- as.character(df[[mice]])
-      colnames(CC_names_l2a)[1] <- mice
-      df <- merge(df,CC_names_l2a[1:2],by=mice,all.x=T)
-      df[mice] <- df["Alias"]
+  } else if (is.rix ==T ) {
+
+    line2alias.rixbase <- function(df){
+
+      df$dam <- as.character(df$dam)
+      df$sire <- as.character(df$sire)
+
+      colnames(CC_names_l2a)[1] <- "sire"
+      df <- merge(df,CC_names_l2a[1:2],by="sire",all.x=T)
+      df["sire"] <- df["Alias"]
       df <- df[-length(df)]
+
+      colnames(CC_names_l2a)[1] <- "dam"
+      df <- merge(df,CC_names_l2a[1:2],by="dam",all.x=T)
+      df["dam"] <- df["Alias"]
+      df <- df[-length(df)]
+
+      df["RIX"] <- paste(df$dam,df$sire,sep="x")
+      return(df)
     }
 
     if(any("RIX" %in% colnames(df)) && any(!("dam" %in% colnames(df)))) {
@@ -113,18 +114,9 @@ line2alias <- function(df){
       df <- df[-(1:2)]
     } else if(any("dam" %in% colnames(df))){
       df <- line2alias.rixbase(df)
-    } else if(any("Strain" %in% colnames(df))){
-      df$Strain <- gsub("/.*","",df$Strain)
-      df <- line2alias.base(df,"Strain")
-    } else if(any("strain" %in% colnames(df))){
-      df$strain <- gsub("/.*","",df$strain)
-      df <- line2alias.base(df,"strain")
-    } else if(any("Line" %in% colnames(df))){
-      df$Line <- gsub("/.*","",df$Line)
-      df <- line2alias.base(df,"Line")
-    } else if(any("line" %in% colnames(df))){
-      df$line <- gsub("/.*","",df$line)
-      df <- line2alias.base(df,"line")
     }
     return(df)
+  }
 }
+
+
