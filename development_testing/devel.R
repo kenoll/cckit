51db58cff9f3fc91ac9e060b6cd56f78325f1ec1
kenoll <kelseynoll@gmail.com>
2019-10-30 14:11:15 -0400

    development folder

---
+++ development_testing/devel.R
@@ -0,0 +1,15 @@
+setwd("~/Documents/heise_lab/cckit/")
+
+# install.packages("devtools")
+# devtools::install_github("klutometis/roxygen")
+
+library(devtools)
+library(roxygen2)
+
+document()
+?install_github
+?install_github
+
+#nothing in development
+
+
