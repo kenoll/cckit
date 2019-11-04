95c7bb028313d3b9758b970524da6e3fd5406ccf
kenoll <kelseynoll@gmail.com>
2019-11-04 09:14:33 -0500

    updated haploscores4 to not break when plyr is loaded

--- R/haploscores_2.R
+++ R/haploscores_2.R
@@ -216,7 +216,7 @@ haploscores4 <- function (chromo, start, end, allele.effects)
 
   haplos <- haploscores(chromo, start, end, allele.effects) %>%
     group_by(strain,alias,chromosome,allele.effects) %>%
-    summarise(haplotype = paste(haplotype, collapse = ", "),
+    dplyr::summarise(haplotype = paste(haplotype, collapse = ", "),
               start_position = paste(start_position, collapse = ", "),
               end_position = paste(end_position, collapse = ", "),
               founders = paste(founders, collapse = ", "))
